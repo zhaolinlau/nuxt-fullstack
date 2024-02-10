1f0850325456f8e94bbc5fed381aef60fac347b7
Zhaolin Lau <zhaolinlau@gmail.com>
2024-02-10 19:56:41 +0800

    add user create task with realtime

--- pages/index.vue
+++ pages/index.vue
@@ -2,77 +2,104 @@
 definePageMeta({
 	middleware: 'auth'
 })
-const user = useSupabaseUser()
-const supabase = useSupabaseClient()
-const createError = ref('')
-const createSuccess = ref('')
+import { RealtimeChannel } from '@supabase/supabase-js'
+let realtimeChannel = RealtimeChannel
+const client = useSupabaseClient();
+const user = useSupabaseUser();
+const addError = ref('')
+const addSuccess = ref('')
 const loading = ref(false)
-const email = ref('');
-const password = ref('');
+const title = ref('')
+const details = ref('')
 
-const createUser = async () => {
+const addTask = async () => {
 	try {
 		loading.value = true
-		const { error } = await useFetch('/api/createUser', {
-			method: 'post',
-			body: {
-				email,
-				password
-			}
+		const { error } = await client.from('tasks').insert({
+			user_id: user.value.id,
+			title: title.value,
+			details: details.value
 		})
-		if (error) return error
-		createSuccess.value = "Account created."
-		email.value = ''
-		password.value = ''
+		if (error) throw error
+		title.value = ''
+		details.value = ''
+		addSuccess.value = 'Added a new task.'
+		addError.value = ''
 	} catch (error) {
-		createError.value = error.message
-		console.log(error)
+		addError.value = error.message
+		addSuccess.value = ''
 	} finally {
 		loading.value = false
 	}
 }
+
+const { data: tasks, refresh: refreshTasks } = useFetch('/api/tasks')
+
+onMounted(() => {
+	realtimeChannel = client.channel('public:tasks').on(
+		'postgres_changes',
+		{ event: '*', schema: 'public', table: 'tasks' },
+		() => refreshTasks()
+	)
+
+	realtimeChannel.subscribe()
+})
 </script>
 
 <template>
-	<div class="hero is-fullheight-with-navbar">
+	<div class="hero mt-6">
 		<div class="hero-body">
 			<div class="container">
-				<p class="title has-text-centered">Welcome {{ user.email }}</p>
-				<form class="box" @submit.prevent="createUser" v-if="user.role == 'service_role'">
+				<div class="columns is-multiline">
+					<div class="column is-12">
+						<form class="box" @submit.prevent="addTask">
 
-					<div class="notification is-success is-light" v-if="createSuccess">
-						<button class="delete" @click="createSuccess = ''"></button>
-						{{ createSuccess }}
+							<div class="notification is-success is-light" v-if="addSuccess">
+								<button class="delete" @click="addSuccess = ''"></button>
+								{{ addSuccess }}
+							</div>
+
+							<div class="notification is-danger is-light" v-if="addError">
+								<button class="delete" @click="addError = ''"></button>
+								{{ addError }}
+							</div>
+
+							<div class="field">
+								<label class="label" for="title">Task Title</label>
+								<div class="control">
+									<input type="text" id="title" class="input" v-model="title" required>
+								</div>
+							</div>
+
+							<div class="field">
+								<label class="label" for="details">Task Details</label>
+								<div class="control">
+									<textarea id="details" class="textarea" v-model="details" required></textarea>
+								</div>
+							</div>
+
+							<div class="field">
+								<div class="control buttons">
+									<button class="button is-primary is-fullwidth" :class="{ 'is-loading': loading }" type="submit">Add
+										Task</button>
+								</div>
+							</div>
+						</form>
 					</div>
 
-					<div class="notification is-danger is-light" v-if="createError">
-						<button class="delete" @click="createError = ''"></button>
-						{{ createError }}
-					</div>
-
-					<div class="field">
-						<label class="label" for="email">Email</label>
-						<div class="control">
-							<input type="email" id="email" class="input" v-model="email" required>
+					<div class="column is-12" v-for="task in tasks">
+						<div class="card">
+							<div class="card-header">
+								<div class="card-header-title">
+									{{ task.title }}
+								</div>
+							</div>
+							<div class="card-content">
+								{{ task.details }}
+							</div>
 						</div>
 					</div>
-
-					<div class="field">
-						<label for="password" class="label">Password</label>
-						<div class="control">
-							<input type="password" id="password" v-model="password" class="input" required>
-						</div>
-					</div>
-
-					<div class="field">
-						<div class="control buttons">
-							<button class="button is-primary is-fullwidth" :class="{ 'is-loading': loading }"
-								type="submit">Create</button>
-							<NuxtLink class="button is-link is-fullwidth" to="/login">Back to Login</NuxtLink>
-						</div>
-					</div>
-
-				</form>
+				</div>
 			</div>
 		</div>
 	</div>
