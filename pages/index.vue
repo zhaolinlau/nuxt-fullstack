44d97112aeaf6c505dcfff43eb4dcf5f77f5745a
Zhaolin Lau <zhaolinlau@gmail.com>
2024-02-11 04:42:37 +0800

    add realtime; add complete task; add delete task

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
 definePageMeta({
 	middleware: 'auth'
 })
+
 import { RealtimeChannel } from '@supabase/supabase-js'
 let realtimeChannel = RealtimeChannel
 const client = useSupabaseClient();
@@ -11,29 +12,45 @@ const addSuccess = ref('')
 const loading = ref(false)
 const title = ref('')
 const details = ref('')
+const showTaskForm = ref(false);
 
 const addTask = async () => {
 	try {
 		loading.value = true
-		const { error } = await client.from('tasks').insert({
-			user_id: user.value.id,
-			title: title.value,
-			details: details.value
-		})
-		if (error) throw error
+		const { error } = await client
+			.from("tasks")
+			.insert({
+				user_id: user.value.id,
+				title: title.value,
+				details: details.value
+			});
 		title.value = ''
 		details.value = ''
 		addSuccess.value = 'Added a new task.'
 		addError.value = ''
+		showTaskForm.value = false
+		if (error) throw error
 	} catch (error) {
 		addError.value = error.message
 		addSuccess.value = ''
 	} finally {
 		loading.value = false
+		setTimeout(() => {
+			addSuccess.value = ''
+			addError.value = ''
+		}, 3000)
 	}
 }
 
-const { data: tasks, refresh: refreshTasks } = useFetch('/api/tasks')
+const { data: tasks, refresh: refreshTasks } = useAsyncData('tasks', async () => {
+	const { data } = await client
+		.from("tasks")
+		.select("*")
+		.eq("user_id", user.value.id)
+		.eq('completed', false)
+		.order('created_at', { ascending: false })
+	return data
+})
 
 onMounted(() => {
 	realtimeChannel = client.channel('public:tasks').on(
@@ -44,6 +61,25 @@ onMounted(() => {
 
 	realtimeChannel.subscribe()
 })
+
+onUnmounted(() => {
+	client.removeChannel(realtimeChannel)
+})
+
+const deleteTask = async (task) => {
+	await client.from("tasks")
+		.delete()
+		.eq("id", task.id)
+}
+
+const completeTask = async (task) => {
+	await client.from("tasks")
+		.update({
+			completed: true,
+			updated_at: new Date()
+		})
+		.eq("id", task.id)
+}
 </script>
 
 <template>
@@ -52,41 +88,45 @@ onMounted(() => {
 			<div class="container">
 				<div class="columns is-multiline">
 					<div class="column is-12">
-						<form class="box" @submit.prevent="addTask">
+						<o-button label="New Task" variant="primary" @click="showTaskForm = true" />
+						<o-modal v-model:active="showTaskForm">
+							<form class="box" @submit.prevent="addTask">
 
-							<div class="notification is-success is-light" v-if="addSuccess">
-								<button class="delete" @click="addSuccess = ''"></button>
-								{{ addSuccess }}
-							</div>
-
-							<div class="notification is-danger is-light" v-if="addError">
-								<button class="delete" @click="addError = ''"></button>
-								{{ addError }}
-							</div>
-
-							<div class="field">
-								<label class="label" for="title">Task Title</label>
-								<div class="control">
-									<input type="text" id="title" class="input" v-model="title" required>
+								<div class="notification is-danger is-light" v-if="addError">
+									<button class="delete" @click="addError = ''"></button>
+									{{ addError }}
 								</div>
-							</div>
 
-							<div class="field">
-								<label class="label" for="details">Task Details</label>
-								<div class="control">
-									<textarea id="details" class="textarea" v-model="details" required></textarea>
+								<div class="field">
+									<label class="label" for="title">Task Title</label>
+									<div class="control">
+										<input type="text" id="title" class="input" v-model="title" required>
+									</div>
 								</div>
-							</div>
 
-							<div class="field">
-								<div class="control buttons">
-									<button class="button is-primary is-fullwidth" :class="{ 'is-loading': loading }" type="submit">Add
-										Task</button>
+								<div class="field">
+									<label class="label" for="details">Task Details</label>
+									<div class="control">
+										<textarea id="details" class="textarea" v-model="details" required></textarea>
+									</div>
 								</div>
-							</div>
-						</form>
+
+								<div class="field">
+									<div class="control buttons">
+										<button class="button is-primary is-fullwidth" :class="{ 'is-loading': loading }" type="submit">Add
+											Task</button>
+										<button class="button is-danger is-fullwidth" @click="showTaskForm = false">Close</button>
+									</div>
+								</div>
+							</form>
+						</o-modal>
 					</div>
-
+					<div class="column is-12" v-if="addSuccess">
+						<div class="notification is-success is-light">
+							<button class="delete" @click="addSuccess = ''"></button>
+							{{ addSuccess }}
+						</div>
+					</div>
 					<div class="column is-12" v-for="task in tasks">
 						<div class="card">
 							<div class="card-header">
@@ -96,6 +136,11 @@ onMounted(() => {
 							</div>
 							<div class="card-content">
 								{{ task.details }}
+							</div>
+							<div class="card-footer">
+								<button class="card-footer-item button is-link">Edit</button>
+								<button class="card-footer-item button is-success" @click="completeTask(task)">Complete</button>
+								<button class="card-footer-item button is-danger" @click="deleteTask(task)">Remove</button>
 							</div>
 						</div>
 					</div>
