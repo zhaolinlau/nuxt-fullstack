575f3061b808a2be6cdbfaa5bacc344cc954b669
Zhaolin Lau <zhaolinlau@gmail.com>
2024-09-16 22:29:41 +0800

    add link and shadow to logo

--- layouts/guest.vue
+++ layouts/guest.vue
@@ -1,8 +1,9 @@
 <template>
 	<div class="h-screen content-center">
-		<div class="top-0 absolute m-3">
-			<NuxtImg src="/img/taskori.webp" alt="taskori.webp" width="75" class="rounded-full" />
-		</div>
+		<ULink to="/">
+			<NuxtImg src="/img/taskori.webp" alt="taskori.webp" width="75"
+				class="top-0 absolute m-3 rounded-full shadow shadow-primary" />
+		</ULink>
 		<slot />
 	</div>
 </template>
