e64e031b0f19e2e1bc21974b4af66213c0b91ef0
Zhaolin Lau <zhaolinlau@gmail.com>
2024-09-17 00:11:06 +0800

    add seo meta

--- app.vue
+++ app.vue
@@ -2,6 +2,15 @@
 useHead({
 	title: 'Taskori'
 })
+
+useSeoMeta({
+	title: 'Taskori',
+	ogTitle: 'Taskori',
+	description: 'Taskori is a modern and intuitive web-based task management app designed to help you stay organized and focused. With a clean and minimalistic design, Taskori makes it easy to create, manage, and prioritize your to-do lists from anywhere, at any time, allowing you to streamline your productivity and achieve a better work-life balance.',
+	ogDescription: 'Taskori is a modern and intuitive web-based task management app designed to help you stay organized and focused. With a clean and minimalistic design, Taskori makes it easy to create, manage, and prioritize your to-do lists from anywhere, at any time, allowing you to streamline your productivity and achieve a better work-life balance.',
+	ogImage: '/img/login.png',
+	twitterCard: 'summary_large_image',
+})
 </script>
 
 <template>
