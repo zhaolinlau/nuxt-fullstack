09f1364edb8f78b23eea938efd1c57134be7b216
Zhaolin Lau <zhaolinlau@gmail.com>
2024-09-17 23:56:30 +0800

    rewrite forgot password

---
+++ components/ForgotPasswordForm.vue
@@ -0,0 +1,50 @@
+<script setup>
+const client = useSupabaseClient()
+const config = useRuntimeConfig()
+const toast = useToast()
+
+const sending = ref(false)
+const state = reactive({
+	email: ''
+})
+
+const sendResetLink = async () => {
+	sending.value = true
+	const { error } = await client.auth.resetPasswordForEmail(state.email, {
+		redirectTo: `${config.public.siteURL}/reset_password`
+	})
+
+	if (error) {
+		toast.add({
+			title: 'Error',
+			description: error.message,
+			color: 'red',
+			icon: 'i-mdi-alert'
+		})
+	} else {
+		toast.add({
+			title: 'Success',
+			icon: 'i-mdi-tick',
+			color: 'green',
+			description: 'Password reset email sent.'
+		})
+
+		state.email = ''
+	}
+	sending.value = false
+}
+</script>
+
+<template>
+	<UForm class="border border-primary rounded-lg p-3" :state="state" @submit="sendResetLink">
+		<p class="text-3xl font-semibold mb-3">Forgot Password</p>
+
+		<UFormGroup label="Email" class="mb-3">
+			<UInput icon="i-mdi-email" v-model="state.email" type="text" />
+		</UFormGroup>
+
+		<UButton type="submit" icon="i-mdi-send" class="mb-3" block :loading="sending" label="Send" />
+
+		<UButton type="button" icon="i-mdi-arrow-left" block variant="link" to="/login" label="Back to login" />
+	</UForm>
+</template>
--- pages/forgot_password.vue
+++ pages/forgot_password.vue
@@ -1,76 +1,14 @@
-<template>
-	<div class="columns is-centered">
-		<div class="column is-6-desktop is-12-touch">
-			<form class="box" @submit.prevent="sendOTPLink">
-				<p class="title has-text-centered">
-					One-time Password (OTP)
-				</p>
-
-				<b-notification type="is-success is-light" :message="sendSuccess" v-if="sendSuccess" closable />
-
-				<b-notification type="is-danger is-light" :message="sendError" v-if="sendError" closable />
-
-				<b-field label="Email">
-					<b-input icon="email" ref="emailInput" v-model="email" type="email" required />
-				</b-field>
-
-				<b-field>
-					<b-button rounded type="is-primary" @click="onSubmit" expanded :loading="sending" native-type="submit"
-						label="Send" />
-				</b-field>
-
-				<b-field>
-					<b-button rounded type="is-link" expanded @click="navigateTo('/login')" label="Back to login" />
-				</b-field>
-			</form>
-		</div>
-	</div>
-</template>
-
 <script setup>
-useHead({
-	title: 'To-dos | Forgot Password'
-})
-
 definePageMeta({
 	layout: 'guest',
 	middleware: 'auth'
 })
-
-const client = useSupabaseClient()
-const email = ref('')
-const sending = ref(false)
-const sendSuccess = ref('')
-const sendError = ref('')
-const runtimeConfig = useRuntimeConfig()
-
-const sendOTPLink = async () => {
-	try {
-		sending.value = true
-		const { error } = await client.auth.signInWithOtp({
-			email: email.value,
-			options: {
-				shouldCreateUser: false,
-				emailRedirectTo: `${runtimeConfig.public.siteURL}/confirm`
-			}
-		})
-		if (error) {
-			throw error
-		} else {
-			sendSuccess.value = "We've sent a link to your email, please click the link to login."
-			email.value = ''
-			sendError.value = ''
-		}
-	} catch (error) {
-		sendError.value = error.message
-		sendSuccess.value = ''
-	} finally {
-		sending.value = false
-	}
-}
-
-const emailInput = ref()
-const onSubmit = async () => {
-	await emailInput.value.checkHtml5Validity()
-}
 </script>
+
+<template>
+	<div class="grid grid-cols-1 lg:grid-cols-3">
+		<div class="col-start-2">
+			<ForgotPasswordForm />
+		</div>
+	</div>
+</template>
